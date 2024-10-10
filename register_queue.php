e7a62ca2c84e56a06efe8f80d0fd24c9adf9b60d
Yaman Hannineh <yamanhannineh@gmail.com>
2024-10-10 12:47:07 -0400

    added use amqpmessage

--- register_queue.php
+++ register_queue.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
 $connection = new AMQPStreamConnection('172.29.85.9', 5672, 'test', 'test', 'Sql-Post');
 $channel = $connection->channel();
